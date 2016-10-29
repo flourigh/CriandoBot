d03be4f9f438c87438fd6abd50f806eec272ac43
Tiago Danin <TiagoDanin@outlook.com>
2016-10-29 13:41:15 -0300

    Update GH-Pages

--- script/run.py
+++ script/run.py
@@ -48,7 +48,7 @@ def github():
 				else:
 					os.system('cd sites/{}/ && git add -A && git commit -S -m "Update GH-Pages"'.format(name))
 					status = write_text(text, name)
-					return
+					break
 					if status:
 						os.system('cd sites/{}/ && git add -A && git commit -S -m "Update GH-Pages"'.format(name))
 						print('{} - {} - {}'.format(readme_data, name, 'OK'))
